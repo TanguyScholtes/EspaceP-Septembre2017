33e23f8161276d4da8f1dac007e662b6f66ebec6
Tanguy SCHOLTES <soultaker720@hotmail.com>
2017-08-30 14:36:20 +0200

    Homepage redesign

--- espacep/index.php
+++ espacep/index.php
@@ -25,37 +25,21 @@ get_header(); ?>
 		<h2 class="title">Informations</h2>
 		<div class="informations-wrapper">
 			<div class="info-type">
-				<h3 class="info-type--title">Travailleurs du Sexe / Sex Workers</h3>
-				<div class="info-type--text">
-					<p>Pour tous les renseignements relatifs à la législation du travail dans le milieu du sexe et d'autres informations pratiques. Page disponible également en anglais !</p>
-					<p>This way for all informations about legislation for sex workers and some useful tips. Page available in English !</p>
-				</div>
-				<p class="info-type--button"><a href="informations_workers">Informations Travailleurs du Sexe / Sex Workers</a></p>
+				<p class="info-type--button"><a href="infos_travailleurs">Travailleurs du sexe</a></p>
+				<p class="info-type--text"><a href="infos_workers">Sex Worker speaking english ? Please visit this page.</a></p>
 			</div>
 			<div class="info-type">
-				<h3 class="info-type--title">Générales</h3>
-				<div class="info-type--text">
-					<p>Si vous êtes un curieux ou que vous recherchez juste des informations sur Espace P et la legislation en place, c'est par ici.</p>
-				</div>
-				<p class="info-type--button"><a href="informations_main">Informations générales</a></p>
-			</div>
-
-			<div class="info-type">
-				<h3 class="info-type--title">Etudiants</h3>
-				<div class="info-type--text">
-					<p>Si vous êtes étudiant et cherchez des informations sur la législation, des stages ou juste sur des activités de sensibilisation, c'est ici.</p>
-				</div>
-				<p class="info-type--button"><a href="informations_students">Informations Etudiants</a></p>
+				<p class="info-type--button"><a href="infos_clients">Clients</a></p>
 			</div>
 			<div class="info-type">
-				<h3 class="info-type--title">Clients</h3>
-				<div class="info-type--text">
-					<p>Si vous êtes client, c'est par là.</p>
-				</div>
-				<p class="info-type--button"><a href="informations_clients">Informations Clients</a></p>
+				<p class="info-type--button"><a href="infos_main">Curieux</a></p>
+			</div>
+			<div class="info-type">
+				<p class="info-type--button"><a href="infos_students">Étudiants</a></p>
 			</div>
 		</div>
 	</section>
+
 	<section class="news">
 		<h2 class="title">À la une</h2>
 		<?php $newsList = new WP_Query('post_type=important'); ?>
@@ -63,11 +47,15 @@ get_header(); ?>
 			<ul class="news__list">
 				<?php $i=1; while ( $newsList->have_posts() && i<=1) : $newsList->the_post(); ?>
 					<li class="news__list__item">
+						<h3 class="news-title"><?php the_title(); ?></h3>
 						<?php the_content(); ?>
 					</li>
 				<?php $i++; endwhile; ?>
 			</ul>
 		<?php endif; ?>
+		<div class="more">
+			<p class="button"><a href="actualites">Voir plus d'actualités</a></p>
+		</div>
 	</section>
 
 </div>
