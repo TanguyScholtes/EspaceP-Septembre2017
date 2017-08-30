bb6554775a6cae8aed14ce470a501755fe6cde74
Tanguy SCHOLTES <soultaker720@hotmail.com>
2017-08-30 18:14:00 +0200

    News page rework

--- espacep/archive-actualites.php
+++ espacep/archive-actualites.php
@@ -3,33 +3,23 @@
 <div class="main main__content">
     <h1 class="page-title">Actualités</h1>
 
-    <div class="content-preview">
-        <h2 class="content-preview__title title">Sommaire</h2>
-        <?php $about = new WP_Query('post_type=actualites&order=ASC'); ?>
-        <?php if ( $about->have_posts() ) : ?>
-            <ul class="content-preview__list">
-                <?php while ( $about->have_posts() ) : $about->the_post(); ?>
-                    <li class="content-preview__list__item">
-                        <a class="content-preview__list__link" href="<?php get_permalink(); ?>"><?php the_title(); ?></a>
+    <div>
+        <?php $posts = new WP_Query('post_type=actualites&order=DES');
+            $posts = $posts -> posts;
+        ?>
+        <?php if ( $posts ) : ?>
+            <ul class="link-list">
+                <?php foreach( $posts as $post ):?>
+                    <li class="link-list-item">
+                        <a class="link-list-item__link" href="<?php echo get_post_permalink(); ?>"><?php the_title(); ?></a>
                     </li>
-                <?php endwhile; ?>
+                <?php endforeach; ?>
             </ul>
+        <?php else: ?>
+            <p>Il n'y a aucune actualité à afficher pour l'instant.</p>
         <?php endif; ?>
     </div>
 
-    <div>
-        <?php $about = new WP_Query('post_type=actualites&order=ASC'); ?>
-        <?php if ( $about->have_posts() ) : ?>
-            <?php while ( $about->have_posts() ) : $about->the_post(); ?>
-                <section class="media">
-                    <h2 class="title media__title"><?php the_title(); ?></h2>
-                    <div>
-                        <?php the_content(); ?>
-                    </div>
-                </section>
-            <?php endwhile; ?>
-        <?php endif; ?>
-    </div>
 </div>
 
 <?php get_footer(); ?>
