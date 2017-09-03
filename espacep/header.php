733603c2e5d58a0b3ed35acb75de53fa417a2972
Tanguy SCHOLTES <soultaker720@hotmail.com>
2017-09-03 11:50:08 +0200

    Responsive design for mobile and tablet

--- espacep/header.php
+++ espacep/header.php
@@ -8,7 +8,8 @@
  * @subpackage espacep
  * @since Espace P 0.0.1
  */
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <!--[if lt IE 7]>      <html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html <?php language_attributes(); ?> class="no-js lt-ie9"> <![endif]-->
